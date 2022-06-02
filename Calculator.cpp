b85e331943dab2179694abc40009c19ddbe00ad5
Abhishek0060 <101703990+Abhishek0060@users.noreply.github.com>
2022-06-02 23:12:32 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, r;
+    char ch;
+
+    cout << "Enter operands (a and b): ";
+    cin >> a >> b;
+
+    cout << "Enter the operator :";
+    cin >> ch;
+
+    switch(ch)
+{
+    case '+' : r = a + b;
+               break;
+    case '-' : r = a - b;
+               break;
+    case '*' : r = a * b;
+               break;
+    default: cout << "Invalid Operator!";
+
+}
+    cout<<"Result: "<<r;
+    return 0;
+} 
